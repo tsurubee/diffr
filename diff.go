9af6578165867c44bca83b293eb4c3c36b7c20dc
tsurubee <29603512+tsurubee@users.noreply.github.com>
2019-03-30 09:42:15 +0900

    Use go-diff/diffmatchpatch

--- diff.go
+++ diff.go
@@ -2,9 +2,20 @@ package diffr
 
 import (
 	"fmt"
+	"os"
+	"time"
 	"github.com/sergi/go-diff/diffmatchpatch"
 	"io/ioutil"
 )
+
+type FileDiff struct {
+	OriginName string
+	OriginTime time.Time
+	NewName string
+	NewTime time.Time
+	Header []string
+	Diffs []diffmatchpatch.Diff
+}
 
 func diffMain(origin, new string) int {
 	o, err := ioutil.ReadFile(origin)
@@ -18,8 +29,20 @@ func diffMain(origin, new string) int {
 		return ExitCodeError
 	}
 
+	originInfo, _ := os.Stat(origin)
+	newInfo, _    := os.Stat(new)
+	fileDiff := &FileDiff{
+		OriginName: originInfo.Name(),
+		OriginTime: originInfo.ModTime(),
+		NewName:    newInfo.Name(),
+		NewTime:    newInfo.ModTime(),
+	}
+	fmt.Println(fileDiff)
+
 	dmp := diffmatchpatch.New()
-	diffs := dmp.DiffMain(string(o), string(n), true)
-	fmt.Println(dmp.DiffPrettyText(diffs))
+	chars1, chars2, lineArray := dmp.DiffLinesToChars(string(o), string(n))
+	diffs := dmp.DiffMain(chars1, chars2, false)
+	result := dmp.DiffCharsToLines(diffs, lineArray)
+	fmt.Println(result)
 	return ExitCodeOK
 }
